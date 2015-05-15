37b83e8438551219795a725f0157c28b619c7f34
ziober40 <ziobrowski.bartek@gmail.com>
2015-05-15 16:21:08 +0100

    added mongo not finished

--- delaychecker.py
+++ delaychecker.py
@@ -1,5 +1,7 @@
 __author__ = 'Bartek'
 
+import datetime
+from pymongo import MongoClient
 from webservice import DarwinLdbSession
 from logger import Logger
 
@@ -9,6 +11,9 @@ class CheckDelay(object):
         self.darwin_sesh = DarwinLdbSession(wsdl="https://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx",
                                             api_key="1696aa2c-a886-44d2-8826-dfae56d95d9f")
         self.logger = Logger()
+        client = MongoClient()
+        self.db = client.nrerpi
+        self.collection = self.db.TrainServices
 
     def get_delayed_trains(self, station_name='KDB', rows=4):
         board = self.darwin_sesh.get_station_board(station_name, rows)
@@ -19,6 +24,7 @@ class CheckDelay(object):
                 service = self.darwin_sesh.get_service_details(b.service_id)
                 if str(service.ata) != "None" and str(service.ata) != "On time":
                     delayed_trains.append(b.destination_text)
+                self.save_in_database(b, service)
                 self.logger.log(str(b.destination_text) + " -  ata:" + str(service.ata) + " - atd:" + str(
                     service.atd) + " - crs:" + str(service.crs) + " - disruption reason:" + str(
                     service.disruption_reason) + " - eta:" + str(service.eta) + " - etd:" + str(
@@ -28,3 +34,18 @@ class CheckDelay(object):
                 self.logger.log('connection downtime or different error' + str(e))
                 return delayed_trains
         return delayed_trains
+
+    def save_in_database(self, service, details):
+
+        train_information = {
+            "InputDate": datetime.datetime.utcnow(),
+            "EstimatedTimeArrival": str(service.eta),
+            "EstimatedTimeDeparture": str(service.etd),
+            "Destination": str(service.destination_text),
+            "Origin": str(service.origin_text),
+            "Platform": str(service.platform),
+            "ScheduledTimeArrival": str(service.sta),
+            "ScheduledTimeDeparture": str(service.std),
+
+        }
+        self.collection.insert(train_information)
